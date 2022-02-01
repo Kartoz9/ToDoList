1331838b673fb1031c1489717138582b15deb957
Dima <podolinnyjdima2@gmail.com>
2022-02-01 17:38:24 +0200

    Добавлена админка и редактирование в ней базы данных

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import Todo
 # Register your models here.
+
+@admin.register(Todo) # or admin.site.register(Tod o, TodoAdmin)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['day', 'number', 'month', 'task']
+    list_editable = ['task', 'number', 'month']
+    ordering = ['month', 'number']
+    list_per_page = 7
--- todo_list/urls.py
+++ todo_list/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = 'Управление TODO list-ом'
+admin.site.index_title = 'Админка сайта'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo_app.urls')),
