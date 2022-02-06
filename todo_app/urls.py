ec94257ff4bcc653f48f4bd4981ab12f081ce19f
Dima <podolinnyjdima2@gmail.com>
2022-02-06 17:09:57 +0200

    Изменен вид сайта с помощью шаблонов, изменены функции в файле views.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_all_days, name='menu'),
-    path('diary/<int:id_todo>', views.show_one_day, name='one-day'),
-    path('diary/<int:id_todo>/leave_task', views.leave_task, name='leave-task'),
-    path('calendar', views.show_calendar, name='calendar')
+    path('', views.HomeListView.as_view(), name='menu'),
+    path('diary/<int:pk>', views.DetailView.as_view(), name='one-day'), #pk тоже самое что и id
+    path('calendar', views.show_calendar, name='calendar'),
+    path('edit-page', views.edit_page, name='edit-page')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,33 +1,40 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Todo
+from django.views.generic import ListView, DetailView
 from django.urls import reverse
 # Create your views here.
 
 
-def show_all_days(request):
-    todo = Todo.objects.all()
-    return render(request, 'todo_app/all_days.html', {
-        'todo': todo
-    })
+#def show_one_day(request, id_todo: int):
+#    example = Tod o.objects.get(id=id_todo)
+#    return render(request, 'todo_app/one_day.html', {
+#        'example': example,
+#    })
 
 
-def show_one_day(request, id_todo: int):
-    example = Todo.objects.get(id=id_todo)
-    return render(request, 'todo_app/one_day.html', {
-        'example': example,
-    })
+class HomeListView(ListView):           #<img src='{% static "todo_app/img/main.png"%}' width = "25px" height="25">
+    model = Todo
+    template_name = 'todo_app/all_days.html'
+    context_object_name = 'todo'
 
 
-def show_calendar(request):
+class DetailView(DetailView):
+    model = Todo
+    template_name = 'todo_app/one_day.html'
+    context_object_name = 'example'
+
+
+def show_calendar(request):             #<img src='{% static "todo_app/img/calendar.png"%}' width = "25px" height="25">
     return render(request, 'todo_app/calendar.html')
 
 
-def leave_task(request, id_todo: int):
-    a = Todo.objects.get(id=id_todo)
-    a.task_set.create(task=request.POST['text'])
-    return render(request, 'todo_app/one_day.html', {
-        'a': a
-    })
+def edit_page(request):
+    example = Todo.objects.all().order_by('id')
+    template = 'todo_app/edit_page.html'
+    context = {
+        'example': example
+    }
+    return render(request, template, context)
 
 
