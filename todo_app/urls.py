765bce7bee2e395f635d2e92ad49a7d578c74952
Dima <podolinnyjdima2@gmail.com>
2022-02-07 13:35:44 +0200

    Добавлен новый функционал: добавление/изменение/удаление заметок, новый файл forms.py, изменения в файлах urls.py, views.py и в шаблонах

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.HomeListView.as_view(), name='menu'),
     path('diary/<int:pk>', views.DetailView.as_view(), name='one-day'), #pk тоже самое что и id
     path('calendar', views.show_calendar, name='calendar'),
-    path('edit-page', views.edit_page, name='edit-page')
+    path('edit-page', views.edit_page, name='edit-page'),
+    path('update-page/<int:pk>', views.update_page, name='update-page'),
+    path('delete-page/<int:pk>', views.delete_page, name='delete-page')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, redirect
 from .models import Todo
 from django.views.generic import ListView, DetailView
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
-# Create your views here.
+from .forms import TodoForm
 
 
 #def show_one_day(request, id_todo: int):
@@ -29,12 +29,46 @@ def show_calendar(request):             #<img src='{% static "todo_app/img/calen
     return render(request, 'todo_app/calendar.html')
 
 
+@csrf_exempt
 def edit_page(request):
+    success = False
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            success = True
+
     example = Todo.objects.all().order_by('id')
     template = 'todo_app/edit_page.html'
     context = {
-        'example': example
+        'example': example,
+        'form': TodoForm(),
+        'success': success
     }
     return render(request, template, context)
 
 
+@csrf_exempt
+def update_page(request, pk):
+    get_todo = Todo.objects.get(pk=pk)
+    success_update = False
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=get_todo)
+        if form.is_valid():
+            form.save()
+            success_update = True
+
+    template = 'todo_app/edit_page.html'
+    context = {
+        'get_todo': get_todo,
+        'update': True,
+        'form': TodoForm(instance=get_todo),
+        'success_update': success_update
+    }
+    return render(request, template, context)
+
+
+def delete_page(request, pk):
+    get_todo = Todo.objects.get(pk=pk)
+    get_todo.delete()
+    return redirect(reverse('edit-page'))
