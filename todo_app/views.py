ea35a85c6adf8939878a42fc4b544c70edc3e116
Dima <podolinnyjdima2@gmail.com>
2022-02-07 17:38:08 +0200

    Все функции переработані на классы + пару новых фич

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from django.contrib import messages
 from .models import Todo
-from django.views.generic import ListView, DetailView
-from django.views.decorators.csrf import csrf_exempt
-from django.urls import reverse
 from .forms import TodoForm
 
 
-#def show_one_day(request, id_todo: int):
-#    example = Tod o.objects.get(id=id_todo)
-#    return render(request, 'todo_app/one_day.html', {
-#        'example': example,
-#    })
+def show_calendar(request):      #<img src='{% static "todo_app/img/calendar.png"%}' width = "25px" height="25">
+    return render(request, 'todo_app/calendar.html')
 
 
-class HomeListView(ListView):           #<img src='{% static "todo_app/img/main.png"%}' width = "25px" height="25">
+class HomeListView(ListView):   #<img src='{% static "todo_app/img/main.png"%}' width = "25px" height="25">
     model = Todo
     template_name = 'todo_app/all_days.html'
     context_object_name = 'todo'
@@ -25,50 +22,51 @@ class DetailView(DetailView):
     context_object_name = 'example'
 
 
-def show_calendar(request):             #<img src='{% static "todo_app/img/calendar.png"%}' width = "25px" height="25">
-    return render(request, 'todo_app/calendar.html')
+class CustomSuccessMessageMixin:
+
+    @property
+    def success_msg(self):
+        return False
+
+    def form_valid(self, form):
+        messages.success(self.request, self.success_msg)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return '%s?id=%s' % (self.success_url, self.object.id)
 
 
-@csrf_exempt
-def edit_page(request):
-    success = False
-    if request.method == 'POST':
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            success = True
+class TodoCreateViews(CustomSuccessMessageMixin, CreateView):
+    model = Todo
+    template_name = 'todo_app/edit_page.html'
+    form_class = TodoForm
+    success_url = reverse_lazy('edit-page')
+    success_msg = 'Заметка создана'
 
-    example = Todo.objects.all().order_by('id')
-    template = 'todo_app/edit_page.html'
-    context = {
-        'example': example,
-        'form': TodoForm(),
-        'success': success
-    }
-    return render(request, template, context)
+    def get_context_data(self, **kwargs):
+        kwargs['example'] = Todo.objects.all().order_by('id')
+        return super().get_context_data(**kwargs)
 
 
-@csrf_exempt
-def update_page(request, pk):
-    get_todo = Todo.objects.get(pk=pk)
-    success_update = False
-    if request.method == 'POST':
-        form = TodoForm(request.POST, instance=get_todo)
-        if form.is_valid():
-            form.save()
-            success_update = True
+class TodoUpdateView(CustomSuccessMessageMixin, UpdateView):
+    model = Todo
+    template_name = 'todo_app/edit_page.html'
+    form_class = TodoForm
+    success_url = reverse_lazy('edit-page')
+    success_msg = 'Заметка обновлена'
 
-    template = 'todo_app/edit_page.html'
-    context = {
-        'get_todo': get_todo,
-        'update': True,
-        'form': TodoForm(instance=get_todo),
-        'success_update': success_update
-    }
-    return render(request, template, context)
+    def get_context_data(self, **kwargs):
+        kwargs['update'] = True
+        return super().get_context_data(**kwargs)
 
 
-def delete_page(request, pk):
-    get_todo = Todo.objects.get(pk=pk)
-    get_todo.delete()
-    return redirect(reverse('edit-page'))
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = 'todo_app/edit_page.html'
+    success_url = reverse_lazy('edit-page')
+    success_msg = 'Заметка удалена'
+
+    def post(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_msg)
+        return super().post(request)
+
