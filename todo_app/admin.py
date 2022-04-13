80d3b67af48bb3cb1673757573a1126c852be95f
Dima <podolinnyjdima2@gmail.com>
2022-04-13 19:26:56 +0300

    Финиш. Календарь оставил от Google

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from . models import Todo
 # Register your models here.
 
-@admin.register(Todo) # or admin.site.register(Tod o, TodoAdmin)
+
+@admin.register(Todo)  # or admin.site.register(T odo, TodoAdmin)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ['day', 'date', 'task']
     list_editable = ['task']
